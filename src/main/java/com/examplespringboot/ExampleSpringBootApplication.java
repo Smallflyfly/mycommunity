8dcb0cbb5c24b54623b101fbe5d8b2d2647a16d3
Smallflyfly <452639095@QQ.COM>
2019-08-05 17:35:09 +0800

    first commit

---
+++ src/main/java/com/examplespringboot/ExampleSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.examplespringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExampleSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExampleSpringBootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/examplespringboot/exampleSpringBootApplicationTests.java
@@ -0,0 +1,16 @@
+package com.examplespringboot;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class exampleSpringBootApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
